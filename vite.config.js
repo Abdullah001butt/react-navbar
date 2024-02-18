4327fdb25b0fbdf3de212b5a59fe922778bb4349
Abdullah001butt <144618477+Abdullah001butt@users.noreply.github.com>
2024-02-18 18:24:44 +0400

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+    // You can add more build options here
+  },
+  server: {
+    port: 3000, // Specify the port for the development server
+    // You can add more development server options here
+  }
 })
